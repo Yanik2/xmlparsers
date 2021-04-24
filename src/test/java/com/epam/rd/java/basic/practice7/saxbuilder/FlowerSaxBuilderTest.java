b5d2391a165ef7cffd6d8b8eb4eb314be9c52242
Yan Zinchenko <wm_yanik@ukr.net>
2021-04-24 18:57:53 +0300

    thirtyfour

--- src/test/java/com/epam/rd/java/basic/practice7/saxbuilder/FlowerSaxBuilderTest.java
+++ src/test/java/com/epam/rd/java/basic/practice7/saxbuilder/FlowerSaxBuilderTest.java
@@ -2,10 +2,14 @@ package com.epam.rd.java.basic.practice7.saxbuilder;
 
 import com.epam.rd.java.basic.practice7.builders.saxbuilder.FlowerSaxBuilder;
 import com.epam.rd.java.basic.practice7.container.Flowers;
+import com.epam.rd.java.basic.practice7.parsers.saxparser.FlowerSaxParser;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.xml.stream.XMLOutputFactory;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 public class FlowerSaxBuilderTest {
@@ -24,5 +28,13 @@ public class FlowerSaxBuilderTest {
     public void shouldBeInstanceOfXMLOutputFactory() {
         FlowerSaxBuilder b = new FlowerSaxBuilder(null);
         Assert.assertTrue(b.getFactory() instanceof XMLOutputFactory);
+    }
+
+    @Test
+    public void shouldCreateFile() throws IOException {
+        FlowerSaxParser p = new FlowerSaxParser();
+        p.parseFlowers("input.xml");
+        FlowerSaxBuilder b = new FlowerSaxBuilder(p.getFlowers());
+        Assert.assertTrue(Files.deleteIfExists(Paths.get("output.sax.xml")));
     }
 }
--- src/test/java/com/epam/rd/java/basic/practice7/staxbuilder/FlowerStaxBuilderTest.java
+++ src/test/java/com/epam/rd/java/basic/practice7/staxbuilder/FlowerStaxBuilderTest.java
@@ -1,14 +1,29 @@
 package com.epam.rd.java.basic.practice7.staxbuilder;
 
+import com.epam.rd.java.basic.practice7.builders.saxbuilder.FlowerSaxBuilder;
 import com.epam.rd.java.basic.practice7.builders.staxbuilder.FlowerStaxBuilder;
 import com.epam.rd.java.basic.practice7.container.Flowers;
+import com.epam.rd.java.basic.practice7.parsers.saxparser.FlowerSaxParser;
+import com.epam.rd.java.basic.practice7.parsers.staxparser.FlowerStaxParser;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FlowerStaxBuilderTest {
     @Test
     public void shouldNotNull() {
         FlowerStaxBuilder b = new FlowerStaxBuilder(new Flowers());
         Assert.assertNotNull(b);
+    }
+
+    @Test
+    public void shouldCreateFile() throws IOException {
+        FlowerStaxParser p = new FlowerStaxParser();
+        p.parseFlowers("input.xml");
+        FlowerStaxBuilder b = new FlowerStaxBuilder(p.getFlowers());
+        Assert.assertTrue(Files.deleteIfExists(Paths.get("output.stax.xml")));
     }
 }
