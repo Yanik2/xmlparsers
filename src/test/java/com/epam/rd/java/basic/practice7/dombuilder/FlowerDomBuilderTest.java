6b2bb53d96ead43ab27a2ef855d2600dcf695ffd
Yan Zinchenko <wm_yanik@ukr.net>
2021-04-24 18:51:30 +0300

    thirtyfour

--- src/test/java/com/epam/rd/java/basic/practice7/dombuilder/FlowerDomBuilderTest.java
+++ src/test/java/com/epam/rd/java/basic/practice7/dombuilder/FlowerDomBuilderTest.java
@@ -2,8 +2,15 @@ package com.epam.rd.java.basic.practice7.dombuilder;
 
 import com.epam.rd.java.basic.practice7.builders.dombuilder.FlowerDomXmlBuilder;
 import com.epam.rd.java.basic.practice7.container.Flowers;
+import com.epam.rd.java.basic.practice7.parsers.domparser.FlowerDomParser;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FlowerDomBuilderTest {
     @Test
@@ -15,5 +22,24 @@ public class FlowerDomBuilderTest {
     @Test
     public void shouldReturnLogger() {
         Assert.assertNotNull(FlowerDomXmlBuilder.getLOGGER());
+    }
+
+    @Test
+    public void shouldCreateFile() {
+        FlowerDomParser p = new FlowerDomParser();
+        p.parseFlowers("input.xml");
+        FlowerDomXmlBuilder b = new FlowerDomXmlBuilder(p.getFlowers());
+        b.buildXml();
+        File file = new File("output.dom.xml");
+        Assert.assertTrue(file.delete());
+    }
+
+    @After
+    public void deleteFile() {
+        try {
+            Files.deleteIfExists(Paths.get("output.dom.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
