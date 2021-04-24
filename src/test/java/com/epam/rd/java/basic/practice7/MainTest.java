73a6a244c1832b6d0ba2f03d340316cfbacbeefe
Yan Zinchenko <wm_yanik@ukr.net>
2021-04-24 19:21:18 +0300

    thirtyseven

--- src/test/java/com/epam/rd/java/basic/practice7/MainTest.java
+++ src/test/java/com/epam/rd/java/basic/practice7/MainTest.java
@@ -1,11 +1,21 @@
 package com.epam.rd.java.basic.practice7;
 
+import com.epam.rd.java.basic.practice7.container.Flowers;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MainTest {
     @Test
     public void shouldTrue() {
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void doesContainerExists() {
+       Assert.assertFalse(false);
     }
 }
--- src/test/java/com/epam/rd/java/basic/practice7/validator/XmlValidatorTest.java
+++ src/test/java/com/epam/rd/java/basic/practice7/validator/XmlValidatorTest.java
@@ -1,7 +1,11 @@
 package com.epam.rd.java.basic.practice7.validator;
 
 import com.epam.rd.java.basic.practice7.builders.dombuilder.FlowerDomXmlBuilder;
+import com.epam.rd.java.basic.practice7.builders.saxbuilder.FlowerSaxBuilder;
+import com.epam.rd.java.basic.practice7.builders.staxbuilder.FlowerStaxBuilder;
 import com.epam.rd.java.basic.practice7.parsers.domparser.FlowerDomParser;
+import com.epam.rd.java.basic.practice7.parsers.saxparser.FlowerSaxParser;
+import com.epam.rd.java.basic.practice7.parsers.staxparser.FlowerStaxParser;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,7 +22,7 @@ public class XmlValidatorTest {
     }
 
     @Test
-    public void shouldReturnTrueWhenValidatingOutput() {
+    public void shouldReturnTrueWhenValidatingOutputDom() {
         FlowerDomParser p = new FlowerDomParser();
         p.parseFlowers("input.xml");
         FlowerDomXmlBuilder b = new FlowerDomXmlBuilder(p.getFlowers());
@@ -26,10 +30,30 @@ public class XmlValidatorTest {
         Assert.assertTrue(XmlValidator.isValid("output.dom.xml", "input-no-targetNamespace.xsd"));
     }
 
+    @Test
+    public void shouldReturnTrueWhenValidatingOutputSax() {
+        FlowerSaxParser p = new FlowerSaxParser();
+        p.parseFlowers("input.xml");
+        FlowerSaxBuilder b = new FlowerSaxBuilder(p.getFlowers());
+        b.buildXml();
+        Assert.assertTrue(XmlValidator.isValid("output.sax.xml", "input-no-targetNamespace.xsd"));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenValidatingOutputStax() {
+        FlowerStaxParser p = new FlowerStaxParser();
+        p.parseFlowers("input.xml");
+        FlowerStaxBuilder b = new FlowerStaxBuilder(p.getFlowers());
+        b.buildXml();
+        Assert.assertTrue(XmlValidator.isValid("output.stax.xml", "input-no-targetNamespace.xsd"));
+    }
+
     @After
     public void deleteFIle() {
         try {
             Files.deleteIfExists(Paths.get("output.dom.xml"));
+            Files.deleteIfExists(Paths.get("output.sax.xml"));
+            Files.deleteIfExists(Paths.get("output.stax.xml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
