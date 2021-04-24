9e7a0114048576ace3ddab3f3b49711e17675a95
Yan Zinchenko <wm_yanik@ukr.net>
2021-04-24 12:10:27 +0300

    twelve

--- src/main/java/com/epam/rd/java/basic/practice7/Main.java
+++ src/main/java/com/epam/rd/java/basic/practice7/Main.java
@@ -3,10 +3,15 @@ package com.epam.rd.java.basic.practice7;
 import com.epam.rd.java.basic.practice7.builders.dombuilder.FlowerDomXmlBuilder;
 import com.epam.rd.java.basic.practice7.builders.saxbuilder.FlowerSaxBuilder;
 import com.epam.rd.java.basic.practice7.builders.staxbuilder.FlowerStaxBuilder;
+import com.epam.rd.java.basic.practice7.item.Flower;
 import com.epam.rd.java.basic.practice7.parsers.domparser.FlowerDomParser;
 import com.epam.rd.java.basic.practice7.parsers.saxparser.FlowerSaxParser;
 import com.epam.rd.java.basic.practice7.parsers.staxparser.FlowerStaxParser;
 import com.epam.rd.java.basic.practice7.validator.XmlValidator;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public final class Main {
 
@@ -20,6 +25,8 @@ public final class Main {
         FlowerStaxParser p = new FlowerStaxParser();
         p.parseFlowers(args[0]);
 
+        List<Flower> flowers = p.getFlowers();
+        sort1(flowers, (f1, f2) -> f1.getName().compareTo(f2.getName()));
         FlowerDomXmlBuilder builder = new FlowerDomXmlBuilder(p.getFlowers());
         builder.buildXml();
 
@@ -28,5 +35,16 @@ public final class Main {
 
         FlowerStaxBuilder staxBuilder = new FlowerStaxBuilder(p.getFlowers());
         staxBuilder.buildXml();
+    }
+
+    private static void sort1(List<Flower> flowers, Comparator<Flower> comparator) {
+        Collections.sort(flowers, comparator);
+    }
+
+    private static void sort2(List<Flower> flowers, Comparator<Flower> comparator) {
+        Collections.sort(flowers, comparator);
+    }
+    private static void sort3(List<Flower> flowers, Comparator<Flower> comparator) {
+        Collections.sort(flowers, comparator);
     }
 }
