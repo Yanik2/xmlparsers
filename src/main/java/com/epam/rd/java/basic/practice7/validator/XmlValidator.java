6c24e568a4552590923b39e9ff8382ead39711b1
Yan Zinchenko <wm_yanik@ukr.net>
2021-04-24 17:39:35 +0300

    twentyeight

--- src/main/java/com/epam/rd/java/basic/practice7/validator/XmlValidator.java
+++ src/main/java/com/epam/rd/java/basic/practice7/validator/XmlValidator.java
@@ -14,34 +14,45 @@ import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-/** This class is validator for xml files
+/**
+ * This class is validator for xml files
+ *
  * @author Yan Zinchenko
  * @version 0.1
  */
 public class XmlValidator {
     private static final Logger LOGGER = Logger.getLogger(XmlValidator.class.getName());
 
-    private XmlValidator() {}
+    private XmlValidator() {
+    }
+
     public static boolean isValid(String filename, String schemaName) {
         String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
         SchemaFactory factory = SchemaFactory.newInstance(language);
-        File schemaLocation = new File(schemaName);
         try {
+            factory.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+            factory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+            factory.setProperty(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
+            File schemaLocation = new File(schemaName);
+
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
             factory.setProperty(XMLConstants.FEATURE_SECURE_PROCESSING, true);
             Schema schema = factory.newSchema(schemaLocation);
-           Validator validator = schema.newValidator();
-           Source source = new StreamSource(filename);
-           validator.setErrorHandler(new FlowerErrorHandler());
-           validator.setProperty(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-           validator.validate(source);
-        } catch(SAXException | IOException e) {
+            Validator validator = schema.newValidator();
+            validator.setProperty("javax.xml.stream.isSupportingExternalEntities", false);
+            Source source = new StreamSource(filename);
+            validator.setErrorHandler(new FlowerErrorHandler());
+            validator.validate(source);
+        } catch (SAXException | IOException e) {
             LOGGER.severe(e.getMessage());
             return false;
         }
         return true;
     }
 
-    /**Error handler for validator*/
+    /**
+     * Error handler for validator
+     */
     private static class FlowerErrorHandler implements ErrorHandler {
         @Override
         public void warning(SAXParseException e) throws SAXException {
