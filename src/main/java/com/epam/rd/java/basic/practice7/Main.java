f6c3e2da1842a3b4b0d16a78ee8c6bb96c6709c8
Yan Zinchenko <wm_yanik@ukr.net>
2021-04-24 10:55:25 +0300

    sixth

--- src/main/java/com/epam/rd/java/basic/practice7/Main.java
+++ src/main/java/com/epam/rd/java/basic/practice7/Main.java
@@ -10,20 +10,20 @@ import com.epam.rd.java.basic.practice7.validator.XmlValidator;
 public final class Main {
 
     public static void main(final String[] args) {
-//        FlowerDomParser domParser = new FlowerDomParser();
-//        domParser.parseFlowers(args[0]);
-//
-//        FlowerSaxParser saxParser = new FlowerSaxParser();
-//        saxParser.parseFlowers(args[0]);
-//
-//        FlowerStaxParser p = new FlowerStaxParser();
-//        p.parseFlowers(args[0]);
-//
-//        FlowerDomXmlBuilder builder = new FlowerDomXmlBuilder(p.getFlowers());
-//        builder.buildXml();
-//
-//        FlowerSaxBuilder b = new FlowerSaxBuilder(p.getFlowers());
-//        b.buildXml();
+        FlowerDomParser domParser = new FlowerDomParser();
+        domParser.parseFlowers(args[0]);
+
+        FlowerSaxParser saxParser = new FlowerSaxParser();
+        saxParser.parseFlowers(args[0]);
+
+        FlowerStaxParser p = new FlowerStaxParser();
+        p.parseFlowers(args[0]);
+
+        FlowerDomXmlBuilder builder = new FlowerDomXmlBuilder(p.getFlowers());
+        builder.buildXml();
+
+        FlowerSaxBuilder b = new FlowerSaxBuilder(p.getFlowers());
+        b.buildXml();
         XmlValidator.isValid("input.xml", "input.xsd");
 
     }
